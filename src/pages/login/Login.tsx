ef2c735caf847438bea7b52b48988f00576d5d0c
Leandro <leandrodev83@gmail.com>
2023-09-19 00:43:17 -0300

    feat:hooks useEfect e useState

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,32 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleLogin = () => {  
+    useEffect(() => {
+        let nome = prompt('Seu nome');
+        if(nome == 'leandro') {
+            alert(`Seu nome é ${nome}`);
+        } else {
+            alert( `seu nome não é ${nome}`);
+        }
+    }, []);
+
+    const handleLogin = () => {
         console.log(email);
         console.log(password);
-  }
+    }
 
     return (
+
         <div>
             <form>
+
                 <label>
                     <span>Email:</span>
                     <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
                 </label>
 
                 <label>
-                    <span>Senha:</span>
+                    <span>Senha</span>
                     <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
                 </label>
 
-                <button type="button" onClick={handleLogin}>
-                    Salvar
+                <button onClick={handleLogin}>
+                    Entrar
                 </button>
+
             </form>
         </div>
+
     );
 }
